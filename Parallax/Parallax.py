444607ebade46ca20217fd752d7df1b758c5086f
KaBlue <never.saymeme@gmail.com>
2019-05-25 22:26:29 +0200

    Finnished Parallax background

--- Parallax/Parallax.py
+++ Parallax/Parallax.py
@@ -9,7 +9,8 @@ height = 840
 
 screen = pygame.display.set_mode((width,height))
 
-rounded_speed = 10
+rounded_speed = 10 #if you put the sensitivity higher than rounded_speed it has the chance to go backwards
+sensitivity = 10
 
 Pictures = []
 picture = {}
@@ -25,21 +26,25 @@ class Background_Parallax(object):
        self.picture = picture
        self.y = int(y)
        if id == 1:   
-           self.speed = random.uniform(rounded_speed-1, rounded_speed+1)
+           self.speed = random.uniform(rounded_speed-sensitivity, rounded_speed+sensitivity)
+           if self.speed == 0:
+               self.speed = random.uniform(rounded_speed-sensitivity, rounded_speed+sensitivity)
            st = 0
            self.x = 0
-       if id == 2:
-           self.speed = Background[len(Background_second)].speed
-           st = 0
-           self.x = 1910
        print(self.speed)
     def moveleft(self):
         self.x -= self.speed
     def Check_boundry(self):
         if self.x < -1910:
             self.x = 1910
+        elif self.x > 1910:
+            self.x = -1910
     def display(self, screen):
         screen.blit(self.picture, (self.x,self.y))
+        if self.x > 0:
+            screen.blit(self.picture, (self.x-1920,self.y))
+        if self.x <= 0:
+            screen.blit(self.picture, (self.x+1920,self.y))
 
 Background =[]
 Back = Background_Parallax(1, Pictures[0])
@@ -48,21 +53,11 @@ Clouds = Background_Parallax(1, Pictures[1])
 Background.append(Clouds)
 Trees = Background_Parallax(1, Pictures[2], 500)
 Background.append(Trees)
-Background_second =[]
-Back_second = Background_Parallax(2, Pictures[0])
-Background_second.append(Back_second)
-Clouds_second = Background_Parallax(2, Pictures[1])
-Background_second.append(Clouds_second)
-Trees_second = Background_Parallax(2, Pictures[2], 500)
-Background_second.append(Trees_second)
+
 
 while running == True:
     screen.fill(white)
     for layer in Background:
-        layer.moveleft()
-        layer.display(screen)
-        layer.Check_boundry()
-    for layer in Background_second:
         layer.moveleft()
         layer.display(screen)
         layer.Check_boundry()
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Random_Stuff
 Random stuff I work on when I am not working on Platform_Game
 
+For all this projects one finnished I have no intentsions of working on them any further. Only for bugs I will work on them.
+
 First addition is 2D road that could be used in games with little addjustments
 
-Second addition is parallax background still WIP
+Second addition is parallax background still that as of now finished. 
